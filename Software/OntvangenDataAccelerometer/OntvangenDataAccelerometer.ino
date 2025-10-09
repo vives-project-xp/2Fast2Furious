ec01ce87750198edd8ddc0d359445a7fbd10ea06
Sam De Wispelaere <samdewispelaere@gmail.com>
2025-10-09 13:44:50 +0200

    code update

--- Software/OntvangenDataAccelerometer/OntvangenDataAccelerometer.ino
+++ Software/OntvangenDataAccelerometer/OntvangenDataAccelerometer.ino
@@ -1,8 +1,15 @@
 #include <ArduinoBLE.h>
 
+int16_t xi = 0;
+int16_t yi = 0;
+int16_t zi = 0;
+
 BLEDevice peripheral;
 BLECharacteristic accelChar;
 bool connected = false;
+
+// Houd de vorige beweging bij
+String prevState = "";
 
 void setup() {
   Serial.begin(9600);
@@ -52,16 +59,44 @@ void loop() {
       byte data[6];
       accelChar.readValue(data, 6);
 
-      int16_t xi = word(data[1], data[0]);
-      int16_t yi = word(data[3], data[2]);
-      int16_t zi = word(data[5], data[4]);
+      xi = word(data[1], data[0]);
+      yi = word(data[3], data[2]);
+      zi = word(data[5], data[4]);
+    }
 
-      Serial.print("Ontvangen accel (mg): ");
-      Serial.print(xi);
-      Serial.print("\t");
-      Serial.print(yi);
-      Serial.print("\t");
-      Serial.println(zi);
+    // Drempels
+    int singleThreshold = 700; // Voor enkelvoudige richtingen
+    int diagonalThreshold = 300; // Voor diagonale richtingen
+
+    String currentState = "";
+
+    // Eerst diagonalen checken (lagere drempel)
+    if (xi > diagonalThreshold && yi > diagonalThreshold) {
+      currentState = "Rechts-Vooruit";
+    } else if (xi > diagonalThreshold && yi < -diagonalThreshold) {
+      currentState = "Links-Vooruit";
+    } else if (xi < -diagonalThreshold && yi > diagonalThreshold) {
+      currentState = "Rechts-Achteruit";
+    } else if (xi < -diagonalThreshold && yi < -diagonalThreshold) {
+      currentState = "Links-Achteruit";
+    } 
+    // Enkelvoudige richtingen checken (hogere drempel)
+    else if (xi > singleThreshold) {
+      currentState = "Vooruit";
+    } else if (xi < -singleThreshold) {
+      currentState = "Achteruit";
+    } else if (yi > singleThreshold) {
+      currentState = "Rechts";
+    } else if (yi < -singleThreshold) {
+      currentState = "Links";
+    } else {
+      currentState = "Still";
+    }
+
+    // Print alleen als status verandert
+    if (currentState != prevState) {
+      Serial.println(currentState);
+      prevState = currentState;
     }
 
     if (!peripheral.connected()) {
